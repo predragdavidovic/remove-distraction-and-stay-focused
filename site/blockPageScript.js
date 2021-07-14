5451ac5d28da215c28228d33358437c1557e36ee
predragdavidovic <predragdavidovic10@gmail.com>
2021-07-14 23:15:42 +0200

    Add style to blocked page

--- site/blockPageScript.js
+++ site/blockPageScript.js
@@ -1,12 +1,43 @@
-if (typeof newDiv === 'undefined') {
-    let newDiv = document.createElement("div");
-    newDiv.style.zIndex = 99999999;
-    newDiv.style.position = "fixed";
-    newDiv.style.top= "0px";
-    newDiv.style.left = "0px"
-    newDiv.style.height = "100%";
-    newDiv.style.width = "100%";
-    newDiv.style.backgroundColor = 'red';
-    newDiv.classList.add("__distraction-blocker__");
-    document.body.append(newDiv);
+if (!hasClass()) {
+    // create elements
+    const container = document.createElement("div");
+    const paragraphDescription = document.createElement("p");
+    const paragraphMotivation = document.createElement("p");
+    const image = document.createElement('img');
+
+    // Add content to elements
+    paragraphDescription.innerHTML = "Page is blocked until a break timer starts.";
+    paragraphMotivation.innerHTML = "Be prodactive and stay focused!"
+    image.src =  chrome.runtime.getURL("./images/forbiden.png");
+    
+    // style container
+    container.style.zIndex = 99999999;
+    container.style.position = "fixed";
+    container.style.textAlign = "center";
+    container.style.fontSize = "16px";
+    container.style.paddingTop = "100px";
+    container.style.fontFamily = "Arial, Helvetica, sans-serif";
+    container.style.top= "0px";
+    container.style.left = "0px"
+    container.style.height = "100%";
+    container.style.width = "100%";
+    container.style.background = "rgb(228,228,228)";
+    container.style.background = "linear-gradient(0deg, rgba(228,228,228,1) 17%, rgba(231,231,231,1) 47%, rgba(211,211,211,1) 73%, rgba(210,210,210,1) 87%)";
+    image.style.width = "127px";
+    image.style.height = "127px";
+    image.style.margin = "0px auto";
+    container.classList.add("__distraction-blocker__");
+    paragraphDescription.style.margin ="16px 0px";
+    paragraphMotivation.style.margin ="0px 0px";
+    
+    // append elements to container  
+    container.append(image);
+    container.append(paragraphDescription);
+    container.append(paragraphMotivation);
+    document.body.append(container);
+    document.body.classList.add('___distraction-blocker___');
+}
+
+function hasClass() {
+    return document.body.classList.contains('___distraction-blocker___')
 }
